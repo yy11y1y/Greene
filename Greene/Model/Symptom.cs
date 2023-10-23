3f7fb0f06f9fc14d841a7d2a282f96ee1875da8f
yy <410570138@m365.fju.edu.tw>
2023-10-23 19:00:46 +0800

    1023

---
+++ Greene/Model/Symptom.cs
@@ -0,0 +1,12 @@
+﻿using System;
+namespace Greene.Model
+{
+    public class SymptomModel
+    {
+        public int SymptomId { get; set; }
+        public string SymptomName { get; set; }
+        public int Severity { get; set; }
+    }
+
+}
+
